1766f6491e36ddb0db8e216ff3f3ed8bbfd85372
Raghav Malhotra <malhotraraghav699@gmail.com>
2022-08-21 13:20:47 -0400

    added beta

---
+++ beta/scripts/app.js
@@ -0,0 +1,37 @@
+const section1 = document.getElementById('section1')
+
+const section2 = document.getElementById('section2')
+
+const lightBirds = document.querySelectorAll('.light-bird')
+const darkbirds = document.querySelectorAll('.dark-bird')
+
+const textSection = document.querySelector('#text-section')
+const ctaSection = document.querySelector('#cta-section')
+
+window.addEventListener('scroll', function (e) {
+  e.preventDefault()
+  // section1.style.position = 'fixed'
+
+  //
+  if (this.window.scrollY > 0) {
+    //console.log(this.window.scrollY)
+
+    if (section2.getBoundingClientRect().y < 450) {
+      //console.log(section2.getBoundingClientRect().y)
+    }
+
+    lightBirds.forEach(function (bird) {
+      bird.style.transform = `translateX(-${this.window.scrollY * 0.05}px)`
+
+      darkbirds.forEach(function (bird) {
+        bird.style.transform = `translateX(-${this.window.scrollY * 0.1}px)`
+      })
+
+      section1.style.transform = `translateY(-${this.window.scrollY * 0.1}px)`
+
+      textSection.style.transform = `translateX(-${this.window.scrollY * 1}px)`
+
+      ctaSection.style.transform = `translateX(${this.window.scrollY * 1}px)`
+    })
+  }
+})
---
+++ beta/scripts/url.js
@@ -0,0 +1,13 @@
+const weatherappBox = document.getElementById('weather-project')
+
+const instabotappBox = document.getElementById('instagram-bot')
+
+const todoBox = document.getElementById('todo-project')
+
+weatherappBox.addEventListener('click', (e) => {
+  window.location.href = 'https://raghavm.com/weather-app/'
+})
+
+instabotappBox.addEventListener('click', (e) => {
+  window.location.href = 'https://github.com/raghavmalhotra/instagram-bot'
+})
